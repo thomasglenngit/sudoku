345c1016057e9eb2c1095d70c04d0c9c37e00922
Thomas Glenn <59554985+thomasglenngit@users.noreply.github.com>
2020-06-23 12:28:36 -0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import $ from 'jquery';
+
+
+
+import { fileName } from './ping-pong';
+
+$(document).ready(function() {
+  $('#form').submit(function(event) {
+    event.preventDefault();
+    const goal = $('#number').val();
+    const output = triangle(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
